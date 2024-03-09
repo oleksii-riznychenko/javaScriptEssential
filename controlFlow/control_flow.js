36739ae333de583170c84fab794ff1b120bca464
Oleksii Riznychenko <oleksiiriznychenko@gmail.com>
2024-03-09 10:01:38 -0500

    control flow 3

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -1,51 +1,105 @@
-let userRole = "admin";
-let accessLevel;
+// let userRole = "admin";
+// let accessLevel;
 
-if (userRole === "admin") {
-    accessLevel = "Full access granted";
-} else if (userRole === "manager") {
-    accessLevel = "Limited access granted";
-} else {
-    accessLevel = "No access granted";
+// if (userRole === "admin") {
+//     accessLevel = "Full access granted";
+// } else if (userRole === "manager") {
+//     accessLevel = "Limited access granted";
+// } else {
+//     accessLevel = "No access granted";
+// }
+
+// console.log("Access Level:", accessLevel);
+
+// let isLoggedIn = true;
+// let userMessage;
+
+// if (isLoggedIn) {
+//     if (userRole === "admin") {
+//         userMessage = "Welcome, Admin!";
+//     } else {
+//         userMessage = "Welcome, User!";
+//     }
+// } else {
+//     userMessage = "Please log in to access the system.";
+// }
+
+// console.log("User Message:", userMessage);
+
+// let userType = "subscriber";
+// let userCategory;
+
+// switch (userType) {
+//     case "admin":
+//         userCategory = "Administrator";
+//         break;
+//     case "manager":
+//         userCategory = "Manager";
+//         break;
+//     case "subscriber":
+//         userCategory = "Subscriber";
+//         break;
+//     default:
+//         userCategory = "Unknown";
+// }
+
+// console.log("User Category:", userCategory);
+
+// let isAuthenticated = true;
+// let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+// console.log("Authentication Status:", authenticationStatus);
+
+const ROLES = {
+    admin: "admin",
+    manager: "manager",
+    subscriber: "subscriber",
+    unknown: "unknown"
+}
+const ACCESS_LEVEL = {
+    full: "Full access granted",
+    limited: "Limited access granted",
+    no: "No access granted",
 }
 
-console.log("Access Level:", accessLevel);
+const userRole = prompt("Set your role:")
+const isLoggedIn = confirm('Login?')
+const isAuthenticated = isLoggedIn;
+let accessLevel, userMessage, userCategory = ''
+const userType = userRole
 
-let isLoggedIn = true;
-let userMessage;
-
-if (isLoggedIn) {
-    if (userRole === "admin") {
-        userMessage = "Welcome, Admin!";
+if (userRole === ROLES.admin) {
+    accessLevel = ACCESS_LEVEL.full
+    if (isLoggedIn) {
+        userMessage = `Welcome, ${ROLES.admin}!`;
     } else {
-        userMessage = "Welcome, User!";
+        userMessage = "Please log in to access the system.";
+    }
+} else if (userRole === ROLES.manager) {
+    accessLevel = ACCESS_LEVEL.limited
+    if (isLoggedIn) {
+        userMessage = `Welcome, ${ROLES.manager}!`;
+    } else {
+        userMessage = "Please log in to access the system.";
     }
 } else {
-    userMessage = "Please log in to access the system.";
+    accessLevel = ACCESS_LEVEL.no
 }
 
-console.log("User Message:", userMessage);
-
-let userType = "subscriber";
-let userCategory;
-
 switch (userType) {
-    case "admin":
+    case ROLES.admin:
         userCategory = "Administrator";
         break;
-    case "manager":
+    case ROLES.manager:
         userCategory = "Manager";
         break;
-    case "subscriber":
+    case ROLES.subscriber:
         userCategory = "Subscriber";
         break;
     default:
         userCategory = "Unknown";
 }
 
-console.log("User Category:", userCategory);
+const authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
-let isAuthenticated = true;
-let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
-
-console.log("Authentication Status:", authenticationStatus);
+console.log({userRole, isLoggedIn, accessLevel, authenticationStatus, userMessage, userCategory, userType});
